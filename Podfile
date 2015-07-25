15ea3b4e9189c4bcb457687162405db747185cfc
haranicle <haranicle@gmail.com>
2015-07-25 17:18:25 +0900

    add realm

---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '8.0'
+use_frameworks!
+
+target 'AsyncRealmSample' do
+    pod 'RealmSwift'
+end
+
+target 'AsyncRealmSampleTests' do
+
+end
+
---
+++ Pods/Target Support Files/Pods-AsyncRealmSample/Pods-AsyncRealmSample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AsyncRealmSample : NSObject
+@end
+@implementation PodsDummy_Pods_AsyncRealmSample
+@end
