8150a3fe0fba5cb1c6630a0ec060640bd7d900ba
haranicle <haranicle@gmail.com>
2015-07-31 00:17:37 +0900

    add animal

---
+++ AsyncRealmSample/Animal.swift
@@ -0,0 +1,14 @@
+//
+//  Animal.swift
+//  AsyncRealmSample
+//
+//  Created by haranicle on 2015/07/25.
+//  Copyright (c) 2015年 haranicle. All rights reserved.
+//
+
+import RealmSwift
+
+class Animal: Object {
+    dynamic var name = ""
+    dynamic var legCount = 4
+}
